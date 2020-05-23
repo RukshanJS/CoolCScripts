15251d79eef81c36893496345766f7012ec7b372
Rukshan J. Senanayaka <rjsenanayaka@gmail.com>
2020-05-23 16:02:01 +0530

    added quadratic formula

---
+++ quadratic_equation/cmake-build-debug/CMakeFiles/quadratic_equation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadratic_equation.dir/main.c.obj"
+  "libquadratic_equation.dll.a"
+  "quadratic_equation.exe"
+  "quadratic_equation.exe.manifest"
+  "quadratic_equation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quadratic_equation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
